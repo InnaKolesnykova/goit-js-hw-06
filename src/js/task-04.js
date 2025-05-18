ca42b0836ba558bcae1d64a30dbf314fd5613706
InnaKolesnykova <149619711+InnaKolesnykova@users.noreply.github.com>
2025-05-18 21:12:11 +0300

    1 day
    5 завдань виконано

---
+++ src/js/task-04.js
@@ -0,0 +1,15 @@
+let counterValue = 0;
+
+const decrementButton = document.querySelector('[data-action="decrement"]');
+const incrementButton = document.querySelector('[data-action="increment"]');
+const value = document.getElementById('value');
+
+decrementButton.addEventListener('click', () => {
+    counterValue -= 1;
+    value.textContent = counterValue;
+});
+
+incrementButton.addEventListener('click', () => {
+    counterValue += 1;
+    value.textContent = counterValue;
+});
