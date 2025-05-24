a361d3bff00919a0ed32a479ab35959264b7b9db
InnaKolesnykova <149619711+InnaKolesnykova@users.noreply.github.com>
2025-05-24 14:43:26 +0300

    Update task7
    7-9 завдання дописані

---
+++ src/js/task-07.js
@@ -0,0 +1,4 @@
+const fontSizeControl = document.getElementById('font-size-control');
+   const textSpan = document.getElementById('text');
+   fontSizeControl.addEventListener('input', function() {
+   });
